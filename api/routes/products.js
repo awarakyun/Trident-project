d87d598c86c52a04eca32208813c9fe9ee13876b
awarakyun <akggmpk@gmail.com>
2018-07-27 23:35:42 +0530

    Resolved issue #2 added mongodb

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../../schemas/products/product');
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling Get request to /products'
-    });
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id).then(doc => {
+        console.log(doc);
+        res.status(200).json({
+            doc
+        });
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: err});
+        });
+    
 });
 
 router.post('/', (req, res, next) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then((result => {
+        console.log(result);
+    }));
+
     res.status(200).json({
-        message: 'Handling post request /products'
+        message: 'Handling post request /products',
+        createdProduct: product
     })
 })
 
-router.get('/:id',(req, res, next) => {
-    res.status(200).json({
-        message: `We got an get request in /products with param ${req.params.id}`
+router.get('/',(req, res, next) => {
+    Product.find().then(doc => {
+        console.log(doc);
+        res.status(200).json({
+            doc
+        });
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: err});
+        });
+    
 })
 module.exports = router;
--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const ordersRoute = require('./api/routes/orders');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://Nirvana-livaz:'
++ process.env.MONGO_ATLAS_PW + 
+'@cluster0-shard-00-00-yypvv.mongodb.net:27017,cluster0-shard-00-01-yypvv.mongodb.net:27017,cluster0-shard-00-02-yypvv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+{
+    useNewUrlParser: true 
+}
+);
 
 app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+res.header('Access-Control-Allow-Origin','*');
+res.header('Access-Control-Allow-Headers',
+'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+);
+if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT', 'POST','PATCH', 'DELETE', 'GET');
+    return res.status(200).json({});
+}
+next();
+})
+
 app.use('/products',productRoutes);
 app.use('/orders', ordersRoute);
 
